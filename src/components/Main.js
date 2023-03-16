c4548eadf8cef5a44932c94c818e9cd26323eef7
taedonn <taedonn@outlook.com>
2023-03-16 12:24:49 +0900

    Update index

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import '../css/Main.css';
+
+function Main() {
+  return (
+    <>
+        <div className='wrap bright_mode'>
+            
+        </div>
+    </>
+  );
+}
+
+export default Main;
