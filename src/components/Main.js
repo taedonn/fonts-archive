89a5813ea34c9c34113ffe652239a57472364112
taedonn <taedonn@outlook.com>
2023-03-20 14:38:39 +0900

    Add night mode, Add top button, add mobile layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -34,7 +34,7 @@ function Main() {
                 +'<div class="font_name">'+item[i].c[1].v+'</div>'
                 +'<div class="type_face">'+item[i].c[4].v+'</div>'
                 +'<div class="font_text" style="font-family:'+item[i].c[2].v+';">'+dummyText+'</div>'
-                +'<svg class="close_btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg>'
+                +'<svg class="close_btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg>'
                 +'<div class="font_detail_page_divider"></div>'
                 +'<div class="font_detail_page_wrap">'
                   +'<div class="font_detail_page">'
@@ -75,7 +75,9 @@ function Main() {
                         +'</div>'
                       +'</div>'
                       +'<p class="font_detail_page_title">폰트 두께</p>'
-                      +'<div class="font_weight_wrap"></div>'
+                      +'<div class="font_weight_wrap">'
+                        +'<input class="font_weight_change" type="text" value="'+dummyTextShort+'"/>'
+                      +'</div>'
                       +'<p class="font_detail_page_title">라이센스 사용 범위</p>'
                       +'<table class="license">'
                         +'<tr>'
@@ -203,7 +205,20 @@ function Main() {
             // 폰트 박스
             fontBox[i].addEventListener('click', (e) => {
               if(fontBox[i].classList.contains('clicked')) { return; }
-              for (let o = 0; o < fontBox.length; o++) { fontBox[o].classList.remove('clicked'); fontBox[o].classList.add('fade_in'); setTimeout(function(){fontBox[o].classList.remove('fade_in');},600); }
+
+              // clicked 이벤트 초기화
+              for (let o = 0; o < fontBox.length; o++) { 
+                fontBox[o].classList.remove('clicked');
+                fontBox[o].classList.add('fade_in');
+                setTimeout(function(){fontBox[o].classList.remove('fade_in');},600);
+
+                // 폰트 두께 텍스트 변경 이벤트
+                fontBox[o].getElementsByClassName('font_weight_change')[0].value = dummyTextShort;
+                let fontWeightTxt = fontBox[o].getElementsByClassName('font_weight_txt');
+                for(let e = 0; e < fontWeightTxt.length; e++) { fontWeightTxt[e].innerText = fontBox[o].getElementsByClassName('font_weight_change')[0].value; }
+              }
+
+              // 새 clicked 이벤트
               fontBox[i].classList.add('clicked');
               fontBox[i].classList.add('fade_in');
               setTimeout(function(){fontBox[i].classList.remove('fade_in');},600);
@@ -215,7 +230,18 @@ function Main() {
               fontBox[i].classList.add('clicked');
               fontBox[i].classList.add('fade_in');
               setTimeout(function(){fontBox[i].classList.remove('fade_in');},600);
-            })
+
+              // 폰트 두께 텍스트 변경 이벤트
+              fontBox[i].getElementsByClassName('font_weight_change')[0].value = dummyTextShort;
+              let fontWeightTxt = fontBox[i].getElementsByClassName('font_weight_txt');
+              for(let e = 0; e < fontWeightTxt.length; e++) { fontWeightTxt[e].innerText = fontBox[i].getElementsByClassName('font_weight_change')[0].value; }
+            });
+
+            // 폰트 두께 텍스트 변경 이벤트
+            fontBox[i].getElementsByClassName('font_weight_change')[0].addEventListener('input', (e) => {
+              let fontWeightTxt = fontBox[i].getElementsByClassName('font_weight_txt');
+              for(let o = 0; o < fontWeightTxt.length; o++) { fontWeightTxt[o].innerText = e.target.value; }
+            });
           }
 
           // 뒤로가기 버튼 클릭 이벤트
@@ -276,14 +302,45 @@ function Main() {
     }
   }
 
+  // 사이드 메뉴 접기 / 펼치기
+  const expandChk = (e) => {
+    if(e.target.checked) {
+      document.getElementsByClassName('wrap')[0].classList.remove('expand');
+      document.getElementsByClassName('wrap')[0].classList.add('shrink');
+    }
+    else {
+      document.getElementsByClassName('wrap')[0].classList.remove('shrink');
+      document.getElementsByClassName('wrap')[0].classList.add('expand');
+    }
+  }
+
+  const goUp = () => { window.scrollTo({top:0,behavior:'smooth'}); }
+
+  const goGitHub = () => { window.open('https://github.com/taedonn/fonts-archive','_blank'); }
+
+  const goNightMode = () => {
+    if(document.body.classList.contains('bright_mode')) {
+      document.body.classList.remove('bright_mode');
+      document.body.classList.add('night_mode');
+    }
+    else {
+      document.body.classList.remove('night_mode');
+      document.body.classList.add('bright_mode');
+    }
+  }
+
   return (
     <>
-        <div className='wrap'>
+        <div className={window.innerWidth > 1080 ? 'wrap expand' : 'wrap shrink'}>
             <div className='side_menu'>
+              <div className='expand_btn_wrap'>
+                <input type='checkbox' id='expand_btn' onClick={expandChk} defaultChecked={window.innerWidth > 1080 ? false : true}/>
+                <label htmlFor='expand_btn'>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/></svg>
+                </label>
+              </div>
               <div className='title'>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                  <path d="m2.244 13.081.943-2.803H6.66l.944 2.803H8.86L5.54 3.75H4.322L1 13.081h1.244zm2.7-7.923L6.34 9.314H3.51l1.4-4.156h.034zm9.146 7.027h.035v.896h1.128V8.125c0-1.51-1.114-2.345-2.646-2.345-1.736 0-2.59.916-2.666 2.174h1.108c.068-.718.595-1.19 1.517-1.19.971 0 1.518.52 1.518 1.464v.731H12.19c-1.647.007-2.522.8-2.522 2.058 0 1.319.957 2.18 2.345 2.18 1.06 0 1.716-.43 2.078-1.011zm-1.763.035c-.752 0-1.456-.397-1.456-1.244 0-.65.424-1.115 1.408-1.115h1.805v.834c0 .896-.752 1.525-1.757 1.525z"/>
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m2.244 13.081.943-2.803H6.66l.944 2.803H8.86L5.54 3.75H4.322L1 13.081h1.244zm2.7-7.923L6.34 9.314H3.51l1.4-4.156h.034zm9.146 7.027h.035v.896h1.128V8.125c0-1.51-1.114-2.345-2.646-2.345-1.736 0-2.59.916-2.666 2.174h1.108c.068-.718.595-1.19 1.517-1.19.971 0 1.518.52 1.518 1.464v.731H12.19c-1.647.007-2.522.8-2.522 2.058 0 1.319.957 2.18 2.345 2.18 1.06 0 1.716-.43 2.078-1.011zm-1.763.035c-.752 0-1.456-.397-1.456-1.244 0-.65.424-1.115 1.408-1.115h1.805v.834c0 .896-.752 1.525-1.757 1.525z"/></svg>
               </div>
               <div className='search_bar'>
                 <input type='text' placeholder='Search Fonts' onChange={searchChange}/>
@@ -326,6 +383,17 @@ function Main() {
                 </div>
               </div>
               <div className='font_area_wrap'></div>
+              <div className='profile_fixed'>
+                <div className='profile_star' onClick={goGitHub}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>
+                </div>
+                <div className='profile_color_mode' onClick={goNightMode}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/></svg>
+                </div>
+                <div className='profile_up' onClick={goUp}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/></svg>
+                </div>
+              </div>
             </div>
         </div>
     </>
