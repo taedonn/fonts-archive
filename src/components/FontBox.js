0821072baa5105623c915200bbbe7a74813f0bd1
taeholee235 <taedonn@outlook.com>
2023-04-28 10:51:39 +0900

    Update FontBox.js

--- src/components/FontBox.js
+++ src/components/FontBox.js
@@ -10,11 +10,22 @@ function FontBox(props) {
     const defaultNum = 12;
     const [num, setNum] = useState(defaultNum);
 
+    // 뒤로가기 시 스크롤이 맨밑에 있을 때 데이터 추가
+    useEffect(() => {
+        const handleScrollPos = setInterval(() => {
+            if (window.scrollY !== 0 && (window.scrollY + window.innerHeight) >= document.body.offsetHeight - (window.innerHeight * 0.1)) {
+                setNum(defaultNum + 12);
+                clearInterval(handleScrollPos);
+            }
+        },200);
+        return () => { clearInterval(handleScrollPos); }
+    },[]);
+
     useEffect(() => {
         // 스크롤 이벤트
         const timer = setInterval(() => {
             window.addEventListener('scroll', () => { handleScroll(); });
-            document.addEventListener('touchmove', () => { handleScroll(); })
+            document.addEventListener('touchmove', () => { handleScroll(); });
         },200);
         return () => {
             clearInterval(timer);
@@ -25,6 +36,7 @@ function FontBox(props) {
 
     // 스크롤 이벤트
     const handleScroll = () => {
+        console.log("Y");
         // 셀렉트박스 스크롤 시 체크 해제
         let select1 = document.getElementById("category_1_select");
         let select2 = document.getElementById("category_2_select");
@@ -47,7 +59,7 @@ function FontBox(props) {
                 {
                     props.sortby === "latest"
                     ? props.data.sort(function(a,b) { return b.c[0].v - a.c[0].v; }).slice(0,num).map((dataEach) => (
-                        <Link className="font_box fade_in" to={`/DetailPage/${dataEach.c[0].v}`} key={dataEach.c[0].v}>
+                        <Link className="font_box fade_in" to={`/DetailPage/${dataEach.c[0].v}`} state={{currentNum:num}} key={dataEach.c[0].v}>
                             <div className="font_name" style={{fontFamily:"'"+dataEach.c[2].v+"'",fontWeight:props.fontWeight}}>{dataEach.c[1].v}</div>
                             <div className='font_info_wrap'>
                                 <div className="font_source" style={{fontFamily:"'"+dataEach.c[2].v+"'",fontWeight:props.fontWeight}}><span>by</span> {dataEach.c[4].v}</div>
@@ -57,7 +69,7 @@ function FontBox(props) {
                         </Link>
                     ))
                     : props.data.sort(function(a,b) { return a.c[1].v.localeCompare(b.c[1].v); }).slice(0,num).map((dataEach) => (
-                        <Link className="font_box fade_in" to={`/DetailPage/${dataEach.c[0].v}`} key={dataEach.c[0].v}>
+                        <Link className="font_box fade_in" to={`/DetailPage/${dataEach.c[0].v}`} state={{currentNum:num}} key={dataEach.c[0].v}>
                             <div className="font_name" style={{fontFamily:"'"+dataEach.c[2].v+"'",fontWeight:props.fontWeight}}>{dataEach.c[1].v}</div>
                             <div className='font_info_wrap'>
                                 <div className="font_source" style={{fontFamily:"'"+dataEach.c[2].v+"'",fontWeight:props.fontWeight}}><span>by</span> {dataEach.c[4].v}</div>
