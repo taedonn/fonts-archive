67172562bb359d9302f9dd8758447a9642c1d793
taedonn <taedonn@outlook.com>
2023-05-09 01:47:19 +0900

    update layout

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -7,7 +7,7 @@ export default function Tooltip() {
 
     return (
         <>
-            <div className="absolute z-5 right-[28px] bottom-[16px] flex flex-col justify-start items-start">
+            <div className="absolute z-10 right-[28px] bottom-[16px] flex flex-col justify-start items-start">
                 <div onClick={goGitHub} className="w-[40px] h-[40px] rounded-full relative flex flex-row justify-center items-center cursor-pointer bg-dark-theme-1 drop-shadow-tooltip group">
                     <svg className="w-[18px] h-[18px] fill-dark-theme-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>
                     <div className='w-content h-[40px] rounded-[12px] absolute right-[56px] top-[50%] translate-y-[-50%] hidden flex-row justify-center items-center text-[14px] font-normal leading-[1] px-[20px] pb-[1px] bg-dark-theme-1 text-dark-theme-8 opacity-0 duration-200 drop-shadow-tooltip before:w-[12px] before:h-[12px] before:absolute before:right-0 before:top-[50%] before:translate-x-[50%] before:translate-y-[-50%] before:bg-dark-theme-1 before:rotate-45 group-hover:flex group-hover:animate-tooltip-fade-in'>깃허브 저장소 바로가기</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 // 훅
-import Link from "next/link";
+import client from "@/libs/client";
 
 
 // 컴포넌트
 import Tooltip from "@/components/tooltip";
-import client from "@/libs/client";
 import SideMenu from "@/components/sidemenu";
 
 const Index = ({fonts}:any) => {
@@ -12,6 +11,116 @@ const Index = ({fonts}:any) => {
         <>
             <Tooltip/>
             <SideMenu fonts={fonts}/>
+            <div className='w-[100%] flex flex-col justify-start items-center'>
+                <div className='interface w-[calc(100%-280px)] fixed right-0 top-0 z-2 p-[20px] duration-200'>
+                    <div className='w-[100%] relative'>
+                        <input type='text' placeholder='원하는 문구를 적어보세요' className="w-[100%] font-notosanskr text-[14px] text-normal text-dark-theme-8 leading-none border rounded-full border-dark-theme-4 px-[20px] py-[16px] pl-[52px] bg-transparent duration-200 hover:bg-dark-theme-3/40 focus:bg-dark-theme-3/40"/>
+                        <svg className="w-[16px] h-[16px] absolute left-[24px] top-[50%] translate-y-[-50%] fill-dark-theme-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="m2.244 13.081.943-2.803H6.66l.944 2.803H8.86L5.54 3.75H4.322L1 13.081h1.244zm2.7-7.923L6.34 9.314H3.51l1.4-4.156h.034zm9.146 7.027h.035v.896h1.128V8.125c0-1.51-1.114-2.345-2.646-2.345-1.736 0-2.59.916-2.666 2.174h1.108c.068-.718.595-1.19 1.517-1.19.971 0 1.518.52 1.518 1.464v.731H12.19c-1.647.007-2.522.8-2.522 2.058 0 1.319.957 2.18 2.345 2.18 1.06 0 1.716-.43 2.078-1.011zm-1.763.035c-.752 0-1.456-.397-1.456-1.244 0-.65.424-1.115 1.408-1.115h1.805v.834c0 .896-.752 1.525-1.757 1.525z"/></svg>
+                    </div>
+                    <div className='w-[100%] flex flex-row justify-start items-center mt-[12px]'>
+                        <div className='w-[100%] flex flex-row justify-start items-center'>
+                            <input type='checkbox' id='select-1' className="hidden"/>
+                            <label htmlFor='select-1' className="h-[34px] relative flex flex-row justify-center items-center text-[14px] text-dark-theme-8 leading-none px-[20px] border border-dark-theme-5 rounded-full duration-200 cursor-pointer">
+                                <div className='w-[100%] h-[100%] absolute z-2'></div>
+                                <button className="w-[100%] h-[100%] flex flex-row justify-center items-center text-inherit leading-none text-[14px]">
+                                    언어 선택
+                                    <svg className="w-[8px] rotate-180 ml-[12px] fill-dark-theme-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/></svg>
+                                </button>
+                            </label>
+                            <div className='category_option category_option_1'>
+                                <input className='handle_lang' type='checkbox' id='KR' value='KR'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='KR'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>한국어</span>
+                                </div>
+                                <input className='handle_lang' type='checkbox' id='EN' value='EN'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='EN'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>영어</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='category_box category_2'>
+                            <input type='checkbox' id='category_2_select'/>
+                            <label className='category_2_select' htmlFor='category_2_select'>
+                                <div className='button_wrap'></div>
+                                <button>
+                                    폰트 형태
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/></svg>
+                                </button>
+                            </label>
+                            <div className='category_option category_option_2'>
+                                <input className='handle_type_face' type='checkbox' id='sansSerif' value='Sans Serif'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='sansSerif'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>고딕</span>
+                                </div>
+                                <input className='handle_type_face' type='checkbox' id='serif' value='Serif'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='serif'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>명조</span>
+                                </div>
+                                <input className='handle_type_face' type='checkbox' id='handWriting' value='Hand Writing' />
+                                <div className='chk_box'>
+                                    <label htmlFor='handWriting'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>손글씨</span>
+                                </div>
+                                <input className='handle_type_face' type='checkbox' id='display' value='Display'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='display'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>장식체</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='divider'></div>
+                        <div className='category_box category_3'>
+                            <input type='checkbox' id='category_3_select'/>
+                            <label className='category_3_select' htmlFor='category_3_select'>
+                                <div className='button_wrap'></div>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/></svg>
+                                </button>
+                            </label>
+                            <div className='category_option category_option_3'>
+                                <input className='handle_sortby' type='radio' id='latest' value='latest' name='sortby'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='latest'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>최신순</span>
+                                </div>
+                                <input className='handle_sortby' type='radio' id='name' value='name' name='sortby'/>
+                                <div className='chk_box'>
+                                    <label htmlFor='name'>
+                                        <svg className='chk_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
+                                        <svg className='chk_fill_btn' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
+                                    </label>
+                                    <span>이름순</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
