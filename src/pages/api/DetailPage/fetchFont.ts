b690bef03c315423c602f57ccf55aecf0f4de314
taedonn <taedonn@outlook.com>
2023-06-06 22:35:49 +0900

    Delete fetchFont.ts

--- src/pages/api/DetailPage/fetchFont.ts
+++
@@ -1,9 +0,0 @@
-import client from "@/libs/client";
-
-export async function FetchFont() {
-    const fonts = await client.fonts.findMany({
-        select: { code: true, },
-    });
-
-    return fonts;
-}
