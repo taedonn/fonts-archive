b87ed7cc7467d8391d195f5c9ed896687379ace6
taeholee235 <taedonn@taedonn.com>
2023-06-23 11:15:19 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -464,6 +464,7 @@ export default function Header (
                                     </> : <div className="text-[14px] text-theme-10/80 dark:text-theme-9/80">
                                         <div className="w-[100%] h-px bg-theme-8/80 dark:bg-theme-7/80 my-[10px]"></div>
                                         <div>{user.user_name}<span className="text-theme-8/80 dark:text-theme-7/80"> 님,</span></div>
+                                        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
                                         <a href="/user/info" className="flex flex-row justify-start items-center text-theme-8/80 hover:text-theme-10/80 tlg:hover:text-theme-8/80 dark:text-theme-7/80 hover:dark:text-theme-9/80 tlg:hover:dark:text-theme-7/80 mt-[8px] group">
                                             <svg className="w-[14px] mr-[4px] mb-px fill-theme-8/80 group-hover:fill-theme-10/80 tlg:group-hover:fill-theme-8/80 dark:fill-theme-7/80 group-hover:dark:fill-theme-9/80 tlg:group-hover:dark:fill-theme-7/80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 5.996V14H3s-1 0-1-1 1-4 6-4c.564 0 1.077.038 1.544.107a4.524 4.524 0 0 0-.803.918A10.46 10.46 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h5ZM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2Zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1Z"/></svg>
                                             회원정보
