7d7303c22c540eeeed601a7eb00e2adeca3ef3d7
taeholee235 <taedonn@taedonn.com>
2023-06-27 17:42:13 +0900

    Update fontbox to be more responsive

--- src/components/fontbox.tsx
+++ src/components/fontbox.tsx
@@ -45,8 +45,12 @@ export default function FontBox ({lang, type, sort, searchword, text, num}:{lang
         <>
             <div className='w-[100%] flex flex-col justify-start items-end'>
                 <div className="main-menu w-[100%] relative flex flex-wrap flex-row justify-between items-stretch pt-[10px] pb-[20px]">
-                    {/* 로딩 바 */}
-                    {isLoading ? <div className="w-[100%] pt-[20px] pb-0 flex flex-row justify-center items-center"><span className="loader w-[40px] tlg:w-[36px] h-[40px] tlg:h-[36px]"></span></div> : null}
+                    {/* 로딩 스켈레톤 */}
+                    {
+                        isLoading 
+                        ? <div className="w-[100%] pt-[20px] pb-0 flex flex-row justify-center items-center"><span className="loader w-[40px] tlg:w-[36px] h-[40px] tlg:h-[36px]"></span></div> 
+                        : null
+                    }
 
                     {/* fetchNextPage */}
                     {data && data.pages.map((page) => {
@@ -62,14 +66,14 @@ export default function FontBox ({lang, type, sort, searchword, text, num}:{lang
                                     font_type: string
                                     cdn_url: string
                                 }) => (
-                                    <a aria-label="font-link" href={`/detailpage/${font.code}`} key={font.code} className="w-[calc(25%-8px)] txl:w-[calc(33.3%-6px)] tlg:w-[calc(50%-4px)] tmd:w-[100%] h-[360px] txl:h-[300px] tlg:h-[240px] tmd:h-[220px] block p-[20px] tlg:p-[16px] border border-theme-7 dark:border-theme-4 rounded-[8px] mt-[12px] txl:mt-[10px] cursor-pointer hover:bg-theme-8/60 tlg:hover:bg-transparent hover:dark:bg-theme-3/40 tlg:hover:dark:bg-transparent animate-fontbox-fade-in">
+                                    <a aria-label="font-link" href={`/detailpage/${font.code}`} key={font.code} className="block w-[calc(25%-8px)] tlg:w-[calc(33.3%-6px)] tmd:w-[calc(50%-4px)] txs:w-[100%] h-[22vw] tlg:h-[30vw] tmd:h-[46vw] txs:h-[82vw] p-[1.04vw] tlg:p-[1.95vw] tmd:p-[2.6vw] txs:p-[4.17vw] mt-[12px] txl:mt-[10px] rounded-[8px] border border-theme-7 dark:border-theme-4 hover:bg-theme-8/60 tlg:hover:bg-transparent hover:dark:bg-theme-3/40 tlg:hover:dark:bg-transparent animate-fontbox-fade-in cursor-pointer">
                                         <link href={font.cdn_url} rel="stylesheet" type="text/css" itemProp="url"></link>
-                                        <div style={{fontFamily:"'"+font.font_family+"'"}} className="text-[18px] tlg:text-[16px] text-normal leading-tight mb-[8px] tlg:mb-[6px] text-theme-3 dark:text-theme-8">{font.name}</div>
+                                        <div style={{fontFamily:"'"+font.font_family+"'"}} className="text-[1.04vw] tlg:text-[1.95vw] tmd:text-[2.6vw] txs:text-[4.17vw] mb-[0.42vw] tlg:mb-[0.78vw] tmd:mb-[1.04vw] txs:mb-[1.67vw] text-normal leading-tight text-theme-3 dark:text-theme-8">{font.name}</div>
                                         <div className="flex flex-row justify-start items-center">
-                                            <div style={{fontFamily:"'"+font.font_family+"'"}} className="inline-block text-[14px] tlg:text-[12px] text-normal text-theme-5 dark:text-theme-6 leading-tight"><span className="text-theme-3 dark:text-theme-8">by</span> {font.source}</div>
+                                            <div style={{fontFamily:"'"+font.font_family+"'"}} className="inline-block text-[0.73vw] tlg:text-[1.37] tmd:text-[1.82vw] txs:text-[2.92vw] text-normal text-theme-5 dark:text-theme-6 leading-tight"><span className="text-theme-3 dark:text-theme-8">by</span> {font.source}</div>
                                         </div>
-                                        <div className="w-[100%] h-px my-[16px] tlg:my-[12px] bg-theme-7 dark:bg-theme-5"></div>
-                                        <div style={{fontFamily:"'"+font.font_family+"'"}} className="text-[36px] txl:text-[32px] tlg:text-[24px] text-normal leading-normal overflow-hidden">
+                                        <div className="w-[100%] h-px my-[0.83vw] tlg:my-[1.56vw] tmd:my-[2.08vw] txs:my-[3.33vw] bg-theme-7 dark:bg-theme-5"></div>
+                                        <div style={{fontFamily:"'"+font.font_family+"'"}} className="text-[1.88vw] tlg:text-[3.52vw] tmd:text-[4.69vw] txs:text-[7.5vw] text-normal leading-normal overflow-hidden">
                                             <p className="ellipsed-text text-theme-3 dark:text-theme-8"><DummyText lang={font.lang} text={text} num={num}/></p>
                                         </div>
                                     </a>
@@ -79,9 +83,9 @@ export default function FontBox ({lang, type, sort, searchword, text, num}:{lang
                     })}
 
                     {/* 정렬 맞추기 위한 빈 div */}
-                    <div className="w-[calc(25%-8px)] txl:w-[calc(33.3%-6px)] tlg:w-[calc(50%-4px)] tmd:w-[100%]"></div>
-                    <div className="w-[calc(25%-8px)] txl:w-[calc(33.3%-6px)] tlg:w-[calc(50%-4px)] tmd:w-[100%]"></div>
-                    <div className="w-[calc(25%-8px)] txl:w-[calc(33.3%-6px)] tlg:w-[calc(50%-4px)] tmd:w-[100%]"></div>
+                    <div className="w-[calc(25%-8px)] tlg:w-[calc(33.3%-6px)] tmd:w-[calc(50%-4px)] txs:w-[100%]"></div>
+                    <div className="w-[calc(25%-8px)] tlg:w-[calc(33.3%-6px)] tmd:w-[calc(50%-4px)] txs:w-[100%]"></div>
+                    <div className="w-[calc(25%-8px)] tlg:w-[calc(33.3%-6px)] tmd:w-[calc(50%-4px)] txs:w-[100%]"></div>
 
                     {/* 뷰포트 만날 시 다음 데이터 로딩 */}
                     <span className="w-[100%]" ref={ref}></span>
