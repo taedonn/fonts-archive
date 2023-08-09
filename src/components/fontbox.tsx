cf4e723b718ba14fd83fc15d2fbbe2cd590e593e
taeholee235 <taedonn@taedonn.com>
2023-08-09 11:29:14 +0900

    Add profile image on header button

--- src/components/fontbox.tsx
+++ src/components/fontbox.tsx
@@ -51,6 +51,7 @@ export default function FontBox ({lang, type, sort, user, like, filter, searchwo
 
     // 좋아요 state
     const [alertDisplay, setAlertDisplay] = useState<boolean>(false);
+    const [hoverDisplay, setHoverDisplay] = useState<boolean>(true);
 
     /** 로그인 중이 아닐 때 좋아요 클릭 방지 */
     const handleLikeClick = (e: React.MouseEvent<HTMLInputElement>) => {
@@ -58,6 +59,30 @@ export default function FontBox ({lang, type, sort, user, like, filter, searchwo
             setAlertDisplay(true);
             e.preventDefault();
         }
+    }
+
+    /** 좋아요 버튼 체인지 이벤트 */
+    const handleLikeChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (user !== null) {
+            // 좋아요 버튼 눌렀을 때 호버창 지우기
+            setHoverDisplay(false);
+
+            await axios.post('/api/updatelike', null, { params: { code: e.target.id ,checked: e.target.checked, user_no: user.user_no } })
+            .then(res => {
+                let hoverEl = e.target.nextSibling?.nextSibling as HTMLDivElement;
+                if (res.data === 'liked') { hoverEl.innerText='좋아요 해제'; }
+                else { hoverEl.innerText='좋아요'; }
+
+                // 좋아요 버튼 눌렀을 때 호버창 다시 띄우기
+                setHoverDisplay(true);
+            })
+            .catch(err => console.log(err));
+        }
+    }
+
+    /** 렌더링 시 좋아요 되어있는 폰트들은 체크된 상태로 변경 */
+    const handleDefaultLike = (fontCode: number) => {
+        return like === null ? false : like.some((font: any) => font.font_id === fontCode);
     }
 
     /** 알럿창 닫기 */
@@ -72,20 +97,6 @@ export default function FontBox ({lang, type, sort, user, like, filter, searchwo
         window.addEventListener('scroll', throttledScroll);
         return () => { window.removeEventListener('scroll', throttledScroll); }
     });
-
-    /** 좋아요 버튼 체인지 이벤트 */
-    const handleLikeChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (user !== null) {
-            await axios.post('/api/updatelike', null, { params: { code: e.target.id ,checked: e.target.checked, user_no: user.user_no } })
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err));
-        }
-    }
-
-    /** 렌더링 시 좋아요 되어있는 폰트들은 체크된 상태로 변경 */
-    const handleDefaultLike = (fontCode: number) => {
-        return like === null ? false : like.some((font: any) => font.font_id === fontCode);
-    }
 
     // 폰트 로딩 콜백 - hook
     const FontFaceObserver = require('fontfaceobserver');
@@ -151,12 +162,13 @@ export default function FontBox ({lang, type, sort, user, like, filter, searchwo
                                 }) => (
                                     <a aria-label="font-link" href={`/detailpage/${font.code}`} key={font.code} className="relative block w-[calc(25%-8px)] tlg:w-[calc(33.3%-6px)] tmd:w-[calc(50%-4px)] txs:w-[100%] h-[22vw] tlg:h-[30vw] tmd:h-[46vw] txs:h-[82vw] p-[1.04vw] tlg:p-[1.95vw] tmd:p-[2.6vw] txs:p-[4.17vw] mt-[12px] tlg:mt-[10px] rounded-[8px] border border-theme-7 dark:border-theme-4 hover:bg-theme-8/60 tlg:hover:bg-transparent hover:dark:bg-theme-3/40 tlg:hover:dark:bg-transparent animate-fontbox-fade-in cursor-pointer">
                                         <link href={font.cdn_url} rel="stylesheet" type="text/css" itemProp="url"></link>
-                                        <div className='absolute top-[1.46vw] tlg:top-[2.73vw] tmd:top-[3.65vw] txs:top-[5.83vw] right-[1.04vw] tlg:right-[1.95vw] tmd:right-[2.6vw] txs:right-[4.17vw]'>
+                                        <div className='group absolute z-20 top-[1.46vw] tlg:top-[2.73vw] tmd:top-[3.65vw] txs:top-[5.83vw] right-[1.25vw] tlg:right-[1.95vw] tmd:right-[2.6vw] txs:right-[4.17vw]'>
                                             <input onClick={handleLikeClick} onChange={handleLikeChange} type="checkbox" id={font.code.toString()} className='like hidden' defaultChecked={handleDefaultLike(font.code)}/>
                                             <label htmlFor={font.code.toString()} className='cursor-pointer'>
                                                 <svg className='w-[1.46vw] tlg:w-[2.73vw] tmd:w-[3.65vw] txs:w-[5.83vw] fill-theme-7 dark:fill-theme-4' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/></svg>
                                                 <svg className='w-[1.46vw] tlg:w-[2.73vw] tmd:w-[3.65vw] txs:w-[5.83vw] fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>
                                             </label>
+                                            <div className={`${hoverDisplay === true ? 'group-hover:block' : 'group-hover:hidden'} same-source w-content absolute z-20 left-[50%] top-[-2.4vw] text-[0.73vw] font-medium leading-none px-[0.63vw] py-[0.42vw] rounded-[0.21vw] hidden tlg:group-hover:hidden group-hover:animate-fontbox-zoom-in bg-theme-yellow dark:bg-theme-blue-1 text-theme-3 dark:text-theme-blue-2 after:w-[0.42vw] after:h-[0.42vw] after:bottom-[-0.21vw]`}>{like === null || like.some((likedFont: any) => likedFont.font_id === font.code) === false ? '좋아요' : '좋아요 해제'}</div>
                                         </div>
                                         <div style={{fontFamily:"'"+font.font_family+"'"}} className="text-[1.04vw] tlg:text-[1.95vw] tmd:text-[2.6vw] txs:text-[4.17vw] mb-[0.42vw] tlg:mb-[0.78vw] tmd:mb-[1.04vw] txs:mb-[1.67vw] text-normal leading-tight text-theme-3 dark:text-theme-8">{font.name}</div>
                                         <div className="flex flex-row justify-start items-center">
