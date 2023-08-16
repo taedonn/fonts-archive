062ec488b9d6673f6fe578ed066f9626c098ba3b
taedonn <taedonn@taedonn.com>
2023-08-17 02:10:44 +0900

    Update header

--- src/pages/user/info.tsx
+++ src/pages/user/info.tsx
@@ -386,7 +386,7 @@ const SendEmail = ({params}: any) => {
                                             {/* eslint-disable-next-line @next/next/no-img-element */}
                                             <img className='w-[100%] h-[100%] object-cover rounded-full' src={params.user.profile_img} width={60} height={60} alt='유저 프로필 사진'/>
                                         </>
-                                        : <div className='w-[100%] h-[100%] rounded-full flex items-center dark:bg-theme-blue-2/60'><div className='img-loader'></div></div>
+                                        : <div className='w-[100%] h-[100%] rounded-full flex items-center bg-theme-4 dark:bg-theme-blue-2/60'><div className='img-loader'></div></div>
                                     }
                                     <div className='w-[20px] h-[18px] flex justify-center items-center bg-theme-4 dark:bg-theme-blue-2 border border-theme-yellow dark:border-theme-blue-1 absolute z-10 left-[2px] bottom-[2px] rounded-[6px]'>
                                         <svg className='w-[10px] fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg>
