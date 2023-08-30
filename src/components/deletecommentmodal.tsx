c0c1673fd020fd64c6458a7d146c5b675663fcbf
taeholee235 <taedonn@taedonn.com>
2023-08-30 12:22:39 +0900

    Detail page
    - Update comments function

--- src/components/deletecommentmodal.tsx
+++ src/components/deletecommentmodal.tsx
@@ -82,16 +82,16 @@ export default function DeleteCommentModal(
                             <div className="text-[14px] text-theme-5 dark:text-theme-7 mt-px">댓글 삭제</div>
                             <button onClick={close} className="w-[36px] h-[24px] rounded-[6px] absolute right-[16px] tmd:right-[12px] top-[50%] translate-y-[-50%] text-[10px] leading-none text-theme-5 dark:text-theme-8 bg-theme-8 dark:bg-theme-3/80 hover:dark:bg-theme-4/60 tlg:hover:dark:bg-theme-3/80 hover:drop-shadow-default hover:dark:drop-shadow-dark tlg:hover:drop-shadow-none tlg:hover:dark:drop-shadow-none">ESC</button>
                         </div>
-                        <div className="w-[100%] p-[20px] bg-theme-8 dark:bg-theme-blue-2">
-                            <h2 className="font-bold text-[16px] text-theme-4 dark:text-theme-9 mb-[8px]">댓글을 삭제하시겠습니까?</h2>
-                            <div className='w-[100%] flex flex-row justify-start items-start mb-[4px] text-[12px] text-theme-5 dark:text-theme-7'>
-                                <svg className='w-[12px] mt-[2px] mr-[6px] dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>
+                        <div className="w-[100%] p-[20px] bg-theme-4 dark:bg-theme-blue-2">
+                            <h2 className="font-bold text-[16px] text-theme-9 mb-[8px]">댓글을 삭제하시겠습니까?</h2>
+                            <div className='w-[100%] flex flex-row justify-start items-start mb-[4px] text-[12px] text-theme-7'>
+                                <svg className='w-[12px] mt-[2px] mr-[6px] fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>
                                 <div>삭제된 댓글은 복구되지 않습니다.</div>
                             </div>
                             <div className="w-[100%] h-px bg-theme-5 my-[16px]"></div>
                             <div className="flex justify-between mt-[12px]">
-                                <button onClick={close} className='w-[calc(50%-5px)] h-[40px] pt-px rounded-[8px] flex flex-row justify-center items-center text-[14px] font-medium text-theme-10 dark:text-theme-8 dark:bg-theme-3/80 hover:dark:bg-theme-3 tlg:hover:dark:bg-theme-3/80'>취소</button>
-                                <button onClick={deleteComment} className='w-[calc(50%-5px)] h-[40px] pt-px rounded-[8px] flex flex-row justify-center items-center text-[14px] font-medium text-theme-10 dark:text-theme-blue-2 dark:bg-theme-blue-1/80 hover:dark:bg-theme-blue-1 tlg:hover:dark:bg-theme-blue-1/80'>확인</button>
+                                <button onClick={close} className='w-[calc(50%-5px)] h-[40px] pt-px rounded-[8px] flex flex-row justify-center items-center text-[14px] font-medium text-theme-10 dark:text-theme-8 bg-theme-5/80 hover:bg-theme-5 tlg:hover:bg-theme-5/80 dark:bg-theme-3/80 hover:dark:bg-theme-3 tlg:hover:dark:bg-theme-3/80'>취소</button>
+                                <button onClick={deleteComment} className='w-[calc(50%-5px)] h-[40px] pt-px rounded-[8px] flex flex-row justify-center items-center text-[14px] font-medium text-theme-4 dark:text-theme-blue-2 bg-theme-yellow/80 hover:bg-theme-yellow tlg:hover:bg-theme-yellow/80 dark:bg-theme-blue-1/80 hover:dark:bg-theme-blue-1 tlg:hover:dark:bg-theme-blue-1/80'>확인</button>
                             </div>
                         </div>
                     </div>
