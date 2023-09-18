ec7850a3f5d2086e4549aa96b477564a85159163
taeho <taedonn@taedonn.com>
2023-09-18 10:50:45 +0900

    Update comment list
    - Style select box
    - Style comment search box

--- src/pages/user/comments.tsx
+++ src/pages/user/comments.tsx
@@ -2,11 +2,7 @@
 import { NextSeo } from 'next-seo';
 
 // react hooks
-import React, { useState, useEffect, useRef, ChangeEvent } from 'react';
-
-// hooks
-import axios from 'axios';
-import imageCompression from 'browser-image-compression';
+import React from 'react';
 
 // api
 import { CheckIfSessionExists } from "../api/user/checkifsessionexists";
@@ -14,8 +10,6 @@ import { FetchUserInfo } from "../api/user/fetchuserinfo";
 
 // components
 import Header from "@/components/header";
-import ChangePwModal from '@/components/changepwmodal';
-import DeleteUserModal from '@/components/deleteusermodal';
 
 const SendEmail = ({params}: any) => {
     // 디바이스 체크
@@ -24,23 +18,12 @@ const SendEmail = ({params}: any) => {
     // 빈 함수
     const emptyFn = () => { return; }
 
-    // 폼 state
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [isImgLoading, setIsImgLoading] = useState<boolean>(false);
-    const [isImgError, setisImgError] = useState<boolean>(false);
-    const [nameVal, setNameVal] = useState<string>('');
-    const [nameChk, setNameChk] = useState<string>('');
-    const [alert, setAlert] = useState<string>('');
-    const [alertDisplay, setAlertDisplay] = useState<boolean>(false);
-    const [changePwModalDisplay, setChangePwModalDisplay] = useState<boolean>(false);
-    const [deleteUserModalDisplay, setDeleteUserModalDisplay] = useState<boolean>(false);
-
     return (
         <>
             {/* Head 부분*/}
             <NextSeo 
-                title={"회원정보 · 폰트 아카이브"}
-                description={"회원정보 - 상업용 무료 한글 폰트 아카이브"}
+                title={"댓글 목록 · 폰트 아카이브"}
+                description={"댓글 목록 - 상업용 무료 한글 폰트 아카이브"}
             />
 
             {/* 헤더 */}
@@ -64,47 +47,16 @@ const SendEmail = ({params}: any) => {
             <form onSubmit={e => e.preventDefault()} className='w-[100%] flex flex-col justify-center items-center'>
                 <div className='w-[720px] tmd:w-[100%] flex flex-col justify-center items-start my-[100px] tlg:my-[40px]'>
                     <h2 className='text-[20px] tlg:text-[18px] text-theme-4 dark:text-theme-9 font-medium mb-[12px] tlg:mb-[8px]'>내 댓글 목록</h2>
-                    <div className='w-[100%] p-[20px] rounded-[8px] text-theme-10 dark:text-theme-9 bg-theme-5 dark:bg-theme-3 drop-shadow-default dark:drop-shadow-dark'>
-                        <div className='flex items-center p-[6px] rounded-[8px] dark:bg-theme-4/60'>
-                            <div className='w-content relative flex shrink-0 flex-row justify-start items-center'>
-                                <input type='checkbox' id='select-lang' className="select hidden"/>
-                                <label htmlFor='select-lang' className="h-[36px] tlg:h-[30px] tmd:h-[28px] relative flex flex-row justify-center items-center text-[14px] text-theme-5 dark:text-theme-8 leading-none tracking-normal px-[20px] tlg:px-[16px] tmd:px-[12px] border border-theme-7 dark:border-theme-5 rounded-[8px] cursor-pointer fill-theme-5 dark:fill-theme-8 hover:bg-theme-4 hover:dark:bg-theme-blue-2 hover:border-theme-yellow hover:dark:border-theme-blue-1 hover:text-theme-10/80 hover:dark:text-theme-9 hover:fill-theme-10/80 hover:dark:fill-theme-9 hover:drop-shadow-default hover:dark:drop-shadow-dark">
-                                    <div className='w-[100%] h-[100%] absolute z-10'></div>
-                                    <button className="w-[100%] flex flex-row justify-center items-center text-inherit leading-[32px] text-[14px] tlg:text-[12px] tmd:text-[10px]">
-                                        검색 필터
-                                        <svg className="w-[8px] tlg:w-[6px] rotate-180 ml-[12px] tlg:ml-[8px] fill-inherit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/></svg>
-                                    </button>
-                                </label>
-                                <div id="option-lang" className='option w-[128px] tlg:w-[108px] tmd:w-[92px] absolute z-2 left-[50%] top-[42px] tlg:top-[36px] tmd:top-[34px] translate-x-[-50%] border border-theme-yellow dark:border-theme-blue-1 rounded-[12px] flex flex-col justify-start items-start px-[16px] tlg:px-[14px] tmd:px-[12px] py-[20px] tmd:py-[14px] tlg:py-[16px] bg-theme-4 dark:bg-theme-blue-2 drop-shadow-default dark:drop-shadow-dark'>
-                                    <input type='radio' id="option-lang-all" name="option-lang" value="all" className="option-input hidden"/>
-                                    <div className='flex flex-row justify-start items-center mb-[16px] tlg:mb-[10px]'>
-                                        <label htmlFor='option-lang-all'>
-                                            <svg className='w-[18px] tlg:w-[14px] mr-[10px] tlg:mr-[8px] cursor-pointer fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
-                                            <svg className='w-[18px] tlg:w-[14px] mr-[10px] tlg:mr-[8px] cursor-pointer fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
-                                        </label>
-                                        <span className="text-[14px] tlg:text-[12px] text-theme-10/80 dark:text-theme-9 leading-tight tlg:pb-px">전체</span>
-                                    </div>
-                                    <input type='radio' id="option-lang-kr" name="option-lang" value="kr" className="option-input hidden"/>
-                                    <div className='flex flex-row justify-start items-center mb-[16px] tlg:mb-[10px]'>
-                                        <label htmlFor='option-lang-kr'>
-                                            <svg className='w-[18px] tlg:w-[14px] mr-[10px] tlg:mr-[8px] cursor-pointer fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
-                                            <svg className='w-[18px] tlg:w-[14px] mr-[10px] tlg:mr-[8px] cursor-pointer fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
-                                        </label>
-                                        <span className="text-[14px] tlg:text-[12px] text-theme-10/80 dark:text-theme-9 leading-tight tlg:pb-px">한국어</span>
-                                    </div>
-                                    <input type='radio' id="option-lang-en" name="option-lang" value="en" className="option-input hidden"/>
-                                    <div className='flex flex-row justify-start items-center'>
-                                        <label htmlFor='option-lang-en'>
-                                            <svg className='w-[18px] tlg:w-[14px] mr-[10px] tlg:mr-[8px] cursor-pointer fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>
-                                            <svg className='w-[18px] tlg:w-[14px] mr-[10px] tlg:mr-[8px] cursor-pointer fill-theme-yellow dark:fill-theme-blue-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>
-                                        </label>
-                                        <span className="text-[14px] tlg:text-[12px] text-theme-10/80 dark:text-theme-9 leading-tight tlg:pb-px">영어</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <input type='textbox' className='w-[100%] h-[36px] ml-[8px] px-[12px] text-[14px] bg-transparent border rounded-[8px] border-theme-7 dark:border-theme-5'/>
-                            <button className='w-[80px] h-[36px] ml-[8px] shrink-0 text-[14px] border rounded-[8px] dark:bg-theme-5/60 pb-px'>검색</button>
+                    <div className='w-[100%] p-[20px] rounded-[8px] text-theme-10 dark:text-theme-9 bg-theme-5 dark:bg-theme-3'>
+                        <div className='w-content flex items-center p-[6px] rounded-[6px] bg-theme-6/30 dark:bg-theme-4/60'>
+                            <select className='w-[80px] h-[32px] text-[12px] pt-px px-[14px] bg-transparent rounded-[6px] outline-none border border-theme-6 dark:border-theme-5 cursor-pointer'>
+                                <option defaultChecked>최신순</option>
+                                <option>답글수</option>
+                            </select>
+                            <input type='textbox' className='w-[200px] h-[32px] ml-[8px] px-[12px] text-[12px] bg-transparent border rounded-[6px] border-theme-6 dark:border-theme-5'/>
+                            <button className='w-[68px] h-[32px] ml-[8px] text-[12px] border rounded-[6px] bg-theme-6/50 hover:bg-theme-6/80 dark:bg-theme-5/60 hover:dark:bg-theme-5 tlg:hover:dark:bg-theme-5/60 pb-px'>검색</button>
                         </div>
+                        <div className='w-[100%] h-px bg-theme-6 dark:bg-theme-5 my-[12px]'></div>
                     </div>
                 </div>
             </form>
