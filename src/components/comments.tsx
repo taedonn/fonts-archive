7552245bee82e9973f7ec42599f10d78c4c547f1
taedonn <taedonn@taedonn.com>
2023-11-18 23:32:06 +0900

    Update comments.tsx

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -387,14 +387,14 @@ export default function Comments (
         if (!Kakao.isInitialized()) Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY as string);
 
         // 카카오톡 공유
-        await Kakao.Share.sendDefault({
-            objectType: 'text',
-            text: '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.',
-            link: {
-                mobileWebUrl: 'https://fonts.taedonn.com',
-                webUrl: 'https://fonts.taedonn.com',
-            },
-        });
+        // await Kakao.Share.sendDefault({
+        //     objectType: 'text',
+        //     text: '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.',
+        //     link: {
+        //         mobileWebUrl: 'https://fonts.taedonn.com',
+        //         webUrl: 'https://fonts.taedonn.com',
+        //     },
+        // });
     }
 
     return (
