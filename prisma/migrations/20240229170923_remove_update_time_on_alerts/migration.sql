a23cb72134871acbca23cf260acb2e3d46b4d087
taedonn <taedonn@taedonn.com>
2024-03-01 02:27:10 +0900

    Update prisma schema

---
+++ prisma/migrations/20240229170923_remove_update_time_on_alerts/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `fontsAlert` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `fontsAlert` DROP COLUMN `updated_at`;
